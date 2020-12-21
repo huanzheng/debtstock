d222b63915d5d53e8d2ea7c23bd2011f9e807783
huanzheng <huan.zheng@nowhere.com>
2020-12-21 16:36:50 +0800

    calculated yijialv
    Signed-off-by: huanzheng <huan.zheng@nowhere.com>

--- src/main/java/debtstock/BondMargin.java
+++ src/main/java/debtstock/BondMargin.java
@@ -1,24 +1,37 @@
 package debtstock;
 
 import java.io.IOException;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.github.tusharepro.core.TushareProService;
 import com.github.tusharepro.core.entity.CbBasicEntity;
+import com.github.tusharepro.core.entity.CbDailyEntity;
+import com.github.tusharepro.core.entity.DailyEntity;
 import com.github.tusharepro.core.entity.MarginDetailEntity;
 import com.github.tusharepro.core.http.Request;
 
 public class BondMargin {
-	public CbBasicEntity cb;
-	public MarginDetailEntity margin;
+	public CbBasicEntity cb = null;
+	public MarginDetailEntity margin = null;
+	public CbDailyEntity cbPrice = null;
+	public DailyEntity stockPrice = null;
+	
+	public Double yjlv;
+	
+	public static String FORMAT_INPUTDATE = "yyyyMMdd";
 
-	public static List<BondMargin> getMatches() {
+	public static List<BondMargin> getMatchesOldFashion() {
 		ArrayList<BondMargin> ret = new ArrayList<BondMargin>();
 		List<MarginDetailEntity> margins = null;
 		List<CbBasicEntity> bonds = null;
+		
 		try {
 			margins = TushareProService.marginDetail(new Request<MarginDetailEntity>() {}  // 使用全局配置
 			        .allFields()  // 所有字段
@@ -52,13 +65,12 @@ public class BondMargin {
 			}
 		}
 		return ret.stream()
-				.sorted((l,r) -> l.cb.getTsCode().compareTo(r.cb.getTsCode()))
+				.sorted((l,r) -> r.cb.getTsCode().compareTo(l.cb.getTsCode()))
 				.collect(Collectors.toList());
 	}
 	
-	public static List<BondMargin> getMatches2() {
+	public static List<BondMargin> getMatches() {
 		List<BondMargin> ret = null;
-		List<CbBasicEntity> bonds = null;
 		try {
 			final List<MarginDetailEntity> margins = TushareProService.marginDetail(new Request<MarginDetailEntity>() {}  // 使用全局配置
 			        .allFields()  // 所有字段
@@ -74,10 +86,10 @@ public class BondMargin {
 					.map(cb -> {
 						BondMargin match = new BondMargin();
 						match.cb = cb;
-						match.margin = margins.stream().filter(margin -> margin.getTsCode().equals(cb.getStkCode())).findAny().get();
+						match.margin = margins.stream().filter(margin -> margin.getTsCode().equals(cb.getStkCode())).findFirst().get();
 						return match;
 					})
-					.sorted((l,r) -> l.cb.getTsCode().compareTo(r.cb.getTsCode()))
+					.sorted((l,r) -> r.cb.getTsCode().compareTo(l.cb.getTsCode()))
 					.collect(Collectors.toList());
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
@@ -86,5 +98,86 @@ public class BondMargin {
 		
 		return ret;
 	}
+	
+	public static void fillPrices(List<BondMargin> bondMargins) {
+		final String dateStr = getLastWorkingDayOfMonth(LocalDate.now()).format(DateTimeFormatter.ofPattern(FORMAT_INPUTDATE));
+		try {
+			List<DailyEntity> stockPrices = TushareProService.daily(new Request<DailyEntity>() {}
+												.param("trade_date", dateStr)
+												)
+												.stream()
+												.sorted((l,r) -> r.getTsCode().compareTo(l.getTsCode()))
+										        .collect(Collectors.toList());
+			
+			List<CbDailyEntity> cbPrices = TushareProService.cbDaily(new Request<CbDailyEntity>() {}
+												.param("trade_date", dateStr)
+										        ).stream()  
+												.sorted((l,r) -> r.getTsCode().compareTo(l.getTsCode()))
+										        .collect(Collectors.toList());
+			
+			bondMargins.stream()
+						.forEach(bondMargin -> {
+							Optional<CbDailyEntity> cbDaily = cbPrices.stream().filter(cbPrice -> cbPrice.getTsCode().equals(bondMargin.cb.getTsCode()))
+													.findFirst();
+							if (cbDaily.isPresent()) {
+								bondMargin.cbPrice = cbDaily.get();
+							} else {
+								System.out.println("Cannot find bond trade info for bond " + bondMargin.cb.getTsCode() + " " + bondMargin.cb.getBondFullName());
+							}
+							Optional<DailyEntity> stockDaily = stockPrices.stream().filter(stockPrice -> stockPrice.getTsCode().equals(bondMargin.cb.getStkCode()))
+													.findFirst();
+							if (stockDaily.isPresent()) {
+								bondMargin.stockPrice = stockDaily.get();
+							} else {
+								System.out.println("Cannot find stock trade info for bond " + bondMargin.cb.getTsCode() + " " + bondMargin.cb.getBondFullName());
+							}
+						});
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+	
+	/**
+	  * Method calculates last working day for last day of month as input
+	  * @param lastDayOfMonth
+	  * @return LocalDate instance containing last working day
+	  */
+	public static LocalDate getLastWorkingDayOfMonth(LocalDate lastDayOfMonth) {
+	    LocalDate lastWorkingDayofMonth;
+	    switch (DayOfWeek.of(lastDayOfMonth.get(ChronoField.DAY_OF_WEEK))) {
+	   		case MONDAY:
+	   			lastWorkingDayofMonth = lastDayOfMonth.minusDays(3);
+	   			break;
+	   		case SATURDAY:
+	   			lastWorkingDayofMonth = lastDayOfMonth.minusDays(1);
+	   			break;
+	   		case SUNDAY:
+	   			lastWorkingDayofMonth = lastDayOfMonth.minusDays(2);
+	   			break;
+	   		default:
+	   			lastWorkingDayofMonth = lastDayOfMonth;
+	    }
+	    return lastWorkingDayofMonth;
+	}
+	
+	public static List<BondMargin> calAndSort(List<BondMargin> bondMargins) {
+		List<BondMargin> ret = bondMargins.stream()
+				.filter(bondMargin -> bondMargin.cb != null && bondMargin.cbPrice != null && bondMargin.stockPrice != null)
+				.collect(Collectors.toList());
+		ret.stream().forEach(bondMargin -> bondMargin.calYjl());
+		return ret.stream().sorted((l,r) -> l.yjlv.compareTo(r.yjlv))
+				.collect(Collectors.toList());
+	}
 
+	public void calYjl() {
+		if (cb != null && cbPrice != null && stockPrice != null) {
+			Double cbShares = cb.getPar()/cb.getConvPrice();
+			Double cbSharePrice = cbPrice.getClose()/cbShares;
+			yjlv = (cbSharePrice/stockPrice.getClose() - 1)*100;
+		} else {
+			yjlv = 10000.0;
+			System.out.println("Missing information");
+		}
+	}
 }
--- src/main/java/debtstock/DebtStock.java
+++ src/main/java/debtstock/DebtStock.java
@@ -1,6 +1,6 @@
 package debtstock;
 
-import java.util.ArrayList;
+import java.text.DecimalFormat;
 import java.util.List;
 
 import com.github.tusharepro.core.TusharePro;
@@ -12,20 +12,22 @@ public class DebtStock {
 		        .setToken(Hidden.token);  // 你的token
 
 		TusharePro.setGlobal(builder.build());  // 设置全局配置
-		
-		List<BondMargin> matches = BondMargin.getMatches2();
-		
+
+		List<BondMargin> matches = BondMargin.getMatches();
 		System.out.println("Totally found " + matches.size() + " bond with margins");
-		int i = 0;
-//		for(BondMargin match : matches) {
-//			System.out.println(" " + i + " -------------------------------------------------- ");
-//			System.out.println(match.cb);
-//			System.out.println(match.margin);
-//			System.out.println(" ------------------------------------------------------------ ");
-//			i++;
-//		}
+
+		BondMargin.fillPrices(matches);
+		matches = BondMargin.calAndSort(matches);
+		
+		DecimalFormat df = new DecimalFormat("##.##");
 		for(BondMargin match : matches) {
-			System.out.println(match.cb.getStkCode() + " " + match.cb.getStkShortName() + " " + match.cb.getTsCode() + " " + match.cb.getBondFullName());
+			System.out.println(match.cb.getStkCode() + " " + match.cb.getStkShortName() 
+						+ " " + match.cb.getTsCode() + " " + match.cb.getBondFullName()
+						+ " " + df.format(match.yjlv) + "%"
+						+ " " + match.cb.getPar()
+						+ " " + match.cb.getConvPrice()
+						+ " " + match.cbPrice.getClose()
+						+ " " + match.stockPrice.getClose());
 		}
 	}
 }
